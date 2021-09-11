27787b9b1f73797b935a6427d1e26a5e18d0cfa5
dragon <dragon@fire.com>
2021-09-12 00:31:37 +0800

    feat(core): 添加 expanding cards

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/expanding_cards', component: '@/pages/projects/expanding_cards' },
+  ],
   fastRefresh: {},
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './index.less';
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>50projects50days</h1>
+      <div>
+        <li>
+          <Link to="/expanding_cards">expanding-cards</Link>
+        </li>
+      </div>
     </div>
   );
 }
