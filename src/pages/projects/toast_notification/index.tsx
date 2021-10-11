fae631a3076c8158e99f84aa7d6d4cc39827df18
dragon <dragon@fire.com>
2021-10-11 09:45:48 +0800

    fix(toast_notification): 修复定时器没有销毁的bug

--- src/pages/projects/toast_notification/index.tsx
+++ src/pages/projects/toast_notification/index.tsx
@@ -1,5 +1,49 @@
+import { useCallback } from 'react';
 import { useEffect, useRef, useState } from 'react';
 import styles from './index.less';
+
+function useTimeout(cb: () => void, delay: number) {
+  const ref = useRef<() => void>();
+
+  useEffect(() => {
+    ref.current = cb;
+  }, [cb]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => ref.current!(), delay);
+    return () => clearTimeout(timer);
+  }, [delay]);
+}
+
+let tid: number = 0;
+
+const Toast = (t: {
+  msg: string;
+  id: number;
+  type: string;
+  onRemove: (id: number) => void;
+}) => {
+  useTimeout(() => {
+    t.onRemove(t.id);
+  }, 3000);
+
+  return (
+    <div
+      key={t.id}
+      className={
+        styles.toast +
+        ' ' +
+        (t.type == 'info'
+          ? styles.info
+          : t.type == 'success'
+          ? styles.success
+          : styles.error)
+      }
+    >
+      {t.msg}
+    </div>
+  );
+};
 
 export default () => {
   const messages = [
@@ -8,32 +52,31 @@ export default () => {
     'Message Three',
     'Message Four',
   ];
-
   const types = ['info', 'success', 'error'];
 
-  const [toasts, setToasts] = useState<Array<{ msg: string; type: string }>>(
-    [],
+  const [toasts, setToasts] = useState<
+    Array<{ id: number; msg: string; type: string }>
+  >([]);
+
+  const removeToast = useCallback(
+    (id) => {
+      setToasts((prev) => prev.filter((t) => t.id != id));
+    },
+    [setToasts],
   );
 
   return (
     <div className={styles.container}>
       <div className={styles.toasts}>
-        {toasts.map((t, i) => {
+        {toasts.map((t) => {
           return (
-            <div
-              key={i}
-              className={
-                styles.toast +
-                ' ' +
-                (t.type == 'info'
-                  ? styles.info
-                  : t.type == 'success'
-                  ? styles.success
-                  : styles.error)
-              }
-            >
-              {t.msg}
-            </div>
+            <Toast
+              key={t.id}
+              id={t.id}
+              msg={t.msg}
+              type={t.type}
+              onRemove={removeToast}
+            ></Toast>
           );
         })}
       </div>
@@ -43,13 +86,11 @@ export default () => {
           setToasts([
             ...toasts,
             {
+              id: tid++,
               msg: messages[Math.floor(Math.random() * messages.length)],
               type: types[Math.floor(Math.random() * types.length)],
             },
           ]);
-          setTimeout(() => {
-            setToasts((prev) => [...prev.slice(1)]);
-          }, 3000);
         }}
       >
         Show Notification
