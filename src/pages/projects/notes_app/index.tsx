8d7c310c8860e75b03cbba78e73f4690c23a0463
dragon <dragon@fire.com>
2021-10-12 18:21:43 +0800

    feat(core): 使用组件优化 notes app

--- src/pages/projects/notes_app/index.tsx
+++ src/pages/projects/notes_app/index.tsx
@@ -1,16 +1,61 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useCallback } from 'react';
 import styles from './index.less';
 
 let idx: number = 0;
+
+const Node = (n: { id: number; onDelete: (id: number) => void }) => {
+  const [lock, setLock] = useState<boolean>(false);
+  const [content, setContent] = useState<string>('');
+
+  console.log('refresh', n.id);
+
+  return (
+    <div className={styles.note}>
+      <div className={styles.tools}>
+        <button
+          className={styles.edit}
+          onClick={() => {
+            setLock(!lock);
+          }}
+        >
+          <i>$</i>
+        </button>
+        <button
+          className={styles.delete}
+          onClick={() => {
+            n.onDelete(n.id);
+          }}
+        >
+          <i>x</i>
+        </button>
+      </div>
+      <div className={styles.main + (lock ? '' : ` ${styles.hidden}`)}>
+        <p>{content}</p>
+      </div>
+      <textarea
+        className={lock ? `${styles.hidden}` : ''}
+        onInput={(e: React.FormEvent) => {
+          setContent((e.target as HTMLTextAreaElement).value);
+        }}
+      ></textarea>
+    </div>
+  );
+};
 
 export default () => {
   const [notes, setNotes] = useState<
     {
       id: number;
-      content: string;
-      lock: boolean;
     }[]
   >([]);
+
+  const remoteNode = useCallback(
+    (id) => {
+      setNotes((prev) => prev.filter((p) => p.id != id));
+    },
+    [setNotes],
+  );
 
   return (
     <div className={styles.body}>
@@ -21,8 +66,6 @@ export default () => {
             ...prev,
             {
               id: idx++,
-              content: '',
-              lock: false,
             },
           ]);
         }}
@@ -30,52 +73,7 @@ export default () => {
         <i>+</i> Add note
       </button>
       {notes.map((n) => {
-        return (
-          <div key={n.id} className={styles.note}>
-            <div className={styles.tools}>
-              <button
-                className={styles.edit}
-                onClick={() => {
-                  setNotes(
-                    notes.map((note) =>
-                      note.id === n.id
-                        ? { ...note, lock: !note.lock }
-                        : { ...note },
-                    ),
-                  );
-                }}
-              >
-                <i>$</i>
-              </button>
-              <button
-                className={styles.delete}
-                onClick={() => {
-                  setNotes((prev) => prev.filter((p) => p.id != n.id));
-                }}
-              >
-                <i>x</i>
-              </button>
-            </div>
-            <div className={styles.main + (n.lock ? '' : ` ${styles.hidden}`)}>
-              <p>{n.content}</p>
-            </div>
-            <textarea
-              className={n.lock ? `${styles.hidden}` : ''}
-              onInput={(e: React.FormEvent) => {
-                setNotes(
-                  notes.map((note) =>
-                    note.id === n.id
-                      ? {
-                          ...note,
-                          content: (e.target as HTMLTextAreaElement).value,
-                        }
-                      : { ...note },
-                  ),
-                );
-              }}
-            ></textarea>
-          </div>
-        );
+        return <Node key={n.id} id={n.id} onDelete={remoteNode}></Node>;
       })}
     </div>
   );
